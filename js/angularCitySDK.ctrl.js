578e2e534ca601f98effa949d089189ab8144a39
Tor Johnson <kasigi@gmail.com>
2016-05-16 15:38:55 -0400

    Page basically working

--- js/angularCitySDK.ctrl.js
+++ js/angularCitySDK.ctrl.js
@@ -1,3 +1,101 @@
 angular.module('censusEdu').controller("MainController", function(){
     var vm = this;
+
+    vm.statename = "";
+    vm.stateInput = "";
+    vm.loadingData = false;
+    vm.processingDisplay = "";
+
+    var sdk = new CitySDK();
+    census = sdk.modules.census;
+    census.enable("21ca50e1a3e22cf2b18083748c278199395408ec");
+
+
+    // Populate State Selector
+    vm.states = [];
+    var stateNames = sdk.stateNames();
+    jQuery.each(census.stateCapitals, function (stateabbrev, capitalcoords) {
+        vm.states.push({
+            stateabbrev: stateabbrev,
+            stateName: stateNames[stateabbrev].toTitleCase()
+        });
+    });
+
+
+    // Check State Data for Selection
+    vm.hasStateData = function(){
+        if(vm.stateData.length > 0){
+            return false;
+        }else{
+            return true;
+        }
+    };
+
+
+
+    // Select State Data
+    vm.stateData = [];
+    vm.selectState = function(){
+        vm.loadingData = true;
+        vm.stateData = [];
+
+        vm.processingDisplay = "Requesting Data from Census";
+        var request = {
+            "level": "state",
+            "state": vm.stateInput,
+            "variables": [
+                "education_associates",
+                "education_bachelors",
+                "education_doctorate",
+                "education_ged",
+                "education_high_school",
+                "education_masters",
+                "education_none",
+                "education_professional",
+                "population"
+            ],
+            "api": "acs5",
+            "year": "2014"
+        };
+
+        vm.maxscale = 50;
+
+        census.APIRequest(request, function (response) {
+            vm.maxscale = 50;
+            vm.processingDisplay = "Data Retrieved. Now Processing...";
+
+            if(typeof response.data[0] !="undefined"){
+                var intermediateData = response.data[0];
+                jQuery.each(intermediateData,function (index,value) {
+                    if (index != "population") {
+                        if((value / intermediateData.population) > 0.5){
+                            vm.maxscale = 100;
+                        }
+
+                    }
+                });
+
+
+
+                jQuery.each(intermediateData,function (index,value) {
+                    if(index != "population"){
+
+
+                    var newBlock = {};
+                    newBlock.level = index.replace("_"," ").toTitleCase();
+                    newBlock.perCapita = value / intermediateData.population;
+                    newBlock.perCapitaUnformatted =(Math.round (newBlock.perCapita*100*100) / 100);
+                    newBlock.perCapitaFormatted = newBlock.perCapitaUnformatted + "%";
+
+                    vm.stateData.push(newBlock);
+                    }
+                });
+            }
+
+            vm.processingDisplay = "";
+            vm.loadingData = false;
+        });
+    };// end selectState()
+
 });
+
--- js/angularCitySDKApp.js
+++ js/angularCitySDKApp.js
@@ -1,23 +1,18 @@
-var request = {
-    "level": "county",
-    "lat": "38.9047",
-    "lng": "-77.0164",
-    "variables": [
-        "education_associates",
-        "education_bachelors",
-        "education_doctorate",
-        "education_ged",
-        "education_high_school",
-        "education_masters",
-        "education_none",
-        "education_professional",
-        "population"
-    ],
-    "api": "acs5",
-    "year": "2014"
-};
+angular.module('app', []);
 
+angular.module('app').controller("MainController", function(){
+    var vm = this;
+    vm.title = 'AngularJS Tutorial Example';
+    vm.searchInput = '';
+});
 
 
 
 angular.module('censusEdu', []);
+
+
+
+
+String.prototype.toTitleCase = function () {
+    return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+};
